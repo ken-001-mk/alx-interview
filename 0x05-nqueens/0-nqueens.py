fafa2357b8b4e5af515c9f92ede2c4988253a7fa
ken <mwaskenne@gmail.com>
2023-09-02 09:13:56 +0300

    nqueen done

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,59 +2,23 @@
 
 import sys
 
-def is_safe(board, row, col, n):
-  """Check if there is a queen in the same column
-  """
-  for i in range(row):
-    if board[i][col] == 'Q':
-      return False
+def nqueens(n: int):
 
-  """Check upper-left diagonal"""
-  for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-    if board[i][j] == 'Q':
-      return False
+    matrix = [[0 for x in range(n)] for y in range(n)]
+    print(str(matrix))
 
-  """Check upper-right diagonal"""
-  for i, j in zip(range(row, -1, -1), range(col, n)):
-    if board[i][j] == 'Q':
-      return False
-
-  return True
-
-def solve_nqueens(n):
-  if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-  def backtrack(row):
-    if row == n:
-      solutions.append([''.join(row) for row in board])
-      return
-
-    for col in range(n):
-      if is_safe(board, row, col, n):
-        board[row][col] = 'Q'
-        backtrack(row + 1)
-        board[row][col] = '.'
-
-  board = [['.' for _ in range(n)] for _ in range(n)]
-  solutions = []
-
-  backtrack(0)
-
-  for solution in solutions:
-    print("\n".join(solution))
-    print()
 
 if __name__ == "__main__":
-  if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    if len(sys.argv) > 2 or len(sys.argv) < 2:
+        print("Usage: nqueens N")
+        exit(1)
 
-  try:
-    n = int(sys.argv[1])
-  except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
 
-  solve_nqueens(n)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    nqueens(int(sys.argv[1]))
