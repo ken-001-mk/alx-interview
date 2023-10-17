c8db4fd60be5c8e1a39fbe1af68a5ea545a2d113
ken <mwaskenne@gmail.com>
2023-10-17 19:53:13 +0300

    perimeter updates

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,44 +1,73 @@
 #!/usr/bin/python3
+""" Island Perimeter """
 
-def check_val(x):
-    if (x == 0):
-        return 1
-    return 0
+bound_4 = set()
+bound_3 = set()
+bound_2 = set()
+bound_1 = set()
+
+
+def boundary(grid, i, j):
+    """Find cells with either 4, 3, 2 or 1 exposed boundary and add them to
+        appropriate set
+        Args:
+            grid (list): 2d list
+            i (int): row number
+            j (int): column number
+    """
+    boundaries = 0
+    try:
+        if i == 0:
+            boundaries += 1
+        elif grid[i-1][j] == 0:
+            boundaries += 1
+    except IndexError:
+        boundaries += 1
+    try:
+        if grid[i+1][j] == 0:
+            boundaries += 1
+    except IndexError:
+        boundaries += 1
+    try:
+        if grid[i][j+1] == 0:
+            boundaries += 1
+    except IndexError:
+        boundaries += 1
+    try:
+        if j == 0:
+            boundaries += 1
+        elif grid[i][j-1] == 0:
+            boundaries += 1
+    except IndexError:
+        boundaries += 1
+
+    if boundaries == 1:
+        bound_1.add((i, j))
+    elif boundaries == 2:
+        bound_2.add((i, j))
+    elif boundaries == 3:
+        bound_3.add((i, j))
+    elif boundaries == 4:
+        bound_4.add((i, j))
 
 
 def island_perimeter(grid):
-    """_summary_
-
-    Args:
-        grid (_type_): _description_
     """
-
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "grid must be between 1 and 100"
-
-    x = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                "grid numbers must be 0 or 1"
+    Calculate and return perimeter of island in the grid
+    Args:
+        grid [list] : 2d list of ints either 0 or 1
+    Return:
+        perimeter of island
+    """
+    if grid == []:
+        return 0
+    L = len(grid)
+    w = len(grid[0])
+    for i in range(L):
+        for j in range(w):
             if grid[i][j] == 1:
-                if i-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i-1][j])
-                if j-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i][j-1])
-
-                try:
-                    x += check_val(grid[i+1][j])
-                except IndexError:
-                    x += 1
-                try:
-                    x += check_val(grid[i][j+1])
-                except IndexError:
-                    x += 1
-
-    return x
+                boundary(grid, i, j)
+                if len(bound_4) != 0:
+                    return 4
+    perimeter = (len(bound_3) * 3) + (len(bound_2) * 2) + (len(bound_1))
+    return perimeter
