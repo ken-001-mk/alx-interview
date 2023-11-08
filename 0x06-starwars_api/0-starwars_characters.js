e8f87b33447e818cb7fd44aabbb7f8ca9b3825fe
ken <mwaskenne@gmail.com>
2023-11-08 20:31:36 +0300

    updated starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,48 @@
 !/usr/bin/node
 
-const request = require('request');
+const fetch = require('node-fetch'); // Node.js environment, or you can use the browser's fetch
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+function getMovieCharacters(movieId) {
+  const baseUrl = 'https://swapi-api.hbtn.io/api/films/';
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
-  }
-);
+  // Make a request to the SWAPI API to get information about the specified movie
+  fetch(`${baseUrl}${movieId}/`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch movie data for Movie ID ${movieId}`);
+      }
+      return response.json();
+    })
+    .then((filmData) => {
+      const charactersUrls = filmData.characters;
+
+      // Iterate through the character URLs and fetch character data
+      charactersUrls.forEach((characterUrl) => {
+        fetch(characterUrl)
+          .then((response) => {
+            if (!response.ok) {
+              console.log(`Failed to fetch character data from ${characterUrl}`);
+              return;
+            }
+            return response.json();
+          })
+          .then((characterData) => {
+            const characterName = characterData.name;
+            console.log(characterName);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+const movieId = process.argv[2]; // Get the Movie ID from the command line arguments
+if (!movieId) {
+  console.log('Usage: node star_wars_characters.js <Movie ID>');
+} else {
+  getMovieCharacters(movieId);
+}
